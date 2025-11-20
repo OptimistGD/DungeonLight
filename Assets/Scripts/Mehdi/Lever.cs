f23c8b5fb5bf310d8f50f164cd97e53422ed74cb
Xileane <merochd@gmail.com>
2025-11-20 23:14:46 +0100

    leviers fonctionnels

--- Assets/Scripts/Mehdi/Lever.cs
+++ Assets/Scripts/Mehdi/Lever.cs
@@ -10,8 +10,9 @@ public class Lever : MonoBehaviour
     [Header("ajouter la touche pour activer ce levier")][SerializeField]
     public KeyCode interactingKey = KeyCode.E;
     public bool state;
+    [SerializeField] private GameObject root;
+    public bool isInRange { get; private set; }
     
-
     void Start()
     {
         if (!leverManager.leverList.Contains(this))
@@ -24,10 +25,10 @@ public class Lever : MonoBehaviour
             Debug.Log(interactingKey.ToString());
         }
     } 
-    
-    private void OnTriggerStay(Collider other)
+
+    private void Update()
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (isInRange)
         {
             if (Input.GetKeyDown(interactingKey))
             {
@@ -36,10 +37,39 @@ public class Lever : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                isInRange = true;
+            }
+        }
+    private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                isInRange = false;
+            }
+        }
+
+    
 
     private void SwitchLever()
     {
         state = !state;
+        /*var x = 0f;
+        var y = 90f;
+        var z = -90f;*/
+        
+        switch (state)
+        {
+            case false:
+                root.transform.eulerAngles = new Vector3(0, root.transform.eulerAngles.y, root.transform.eulerAngles.z);
+                break;
+            case true:
+                root.transform.eulerAngles = new Vector3(180, root.transform.eulerAngles.y, root.transform.eulerAngles.z);
+                break;
+        }
         leverManager.LeverSwitched(state);
     }
 }
